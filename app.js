02294d027a10843b60f6efe10e420ba861ca1c41
maytezhou <you@example.com>
2019-08-31 11:24:23 -0500

    learning about handlebars,routes

--- app.js
+++ app.js
@@ -1,34 +1,83 @@
 const express = require ('express');
 const exphbs  = require('express-handlebars');
-
-
-
+const bodyParser=require('body-parser');
+const mongoose= require ('mongoose');
 const app = express();
+//Map global promise - get rid of warning 
+// Map global promise - get rid of warning
+// Map global promise - get rid of warning
+mongoose.Promise = global.Promise;
+// Connect to mongoose
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true}).then(()=>{
+
+}).catch((err)=>{
+console.log(err)
+});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
+
 //Handle bars middleware 
-app.engine('handlebars', exphbs());
+app.engine('handlebars', exphbs({
+    defaultLayout:'main'
+}));
 app.set('view engine', 'handlebars');
 
-//how middleware works
-app.use(function(req,res,next){
-/* console.log(Date.now()); */
-req.name='Mayte Souza';
+//Body parser  middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-next();
-})
+
 
 //Index route 
 app.get('/',(req,res)=>{
-  
-res.render('index');
+  const title='Welcome1';
+
+res.render('index',{
+    title:title});
 });
 
+//Add ideas  ROute
+app.get('/ideas/add',(req,res)=>{
+  res.render('ideas/add');
+  });
+
+
+
 //ABoout ROute
-app.get('/about',(req,resp)=>{
-    resp.render('ABOUT');
+app.get('/about',(req,res)=>{
+    res.render('about');
     });
 
    
-const port = 5000;
+//Process from 
+app.post('/ideas',(req,res)=>{
+  let errors = [];
+  if (!req.body.title){
+errors.push({text:'Please add a title',
+});}
+
+if(!req.body.details){
+  errors.push({text:'Please add some details'});
+}
+if (errors.length >0 ){
+  res.render('ideas/add',{
+    errors:errors,
+    title:req.body.title,
+    details:req.body.details,
+  });
+}else {
+  res.send('passed');
+}
+console.log('this is reqqqquest', req);
+console.log('this is response',  res);
+})
+
+
+const port = 9000;
 
 app.listen(port, ()=>{
     console.log(`Server started on port ${port}`);
